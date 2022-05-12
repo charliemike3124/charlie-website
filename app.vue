0f8c6716cb230635d4db962b3473fcfa82f5bdc9
Carlos <charliemike31245@gmail.com>
2022-05-11 19:42:14 -0500

    create contact button

---
+++ app.vue
@@ -0,0 +1,25 @@
+<template>
+	<nav class="p-5 border-gray-50 border-2">
+		<div class="flex flex-row">
+			<div class="w-1/2">
+				<span>Projects</span>
+				<span>About Me</span>
+			</div>
+			<div class="w-1/2">
+				<ContactBtn @openContactForm="openContactForm"></ContactBtn>
+			</div>
+		</div>
+	</nav>
+	<NuxtLayout name="HomePage">{{ common.test }}</NuxtLayout>
+	<NuxtLayout name="ContactForm"><button @click="common.increment">Contact</button></NuxtLayout>
+</template>
+
+<script setup>
+import { useCommonStore } from './stores/common';
+
+const common = useCommonStore();
+
+function openContactForm() {
+	console.log('open');
+}
+</script>
---
+++ components/ContactBtn.vue
@@ -0,0 +1,7 @@
+<template>
+	<button class="p-5 rounded-lg bg-blue-500 text-white" @click="$emit('openContactForm')">Contact Me</button>
+</template>
+
+<script setup>
+defineEmits(['openContactForm']);
+</script>
